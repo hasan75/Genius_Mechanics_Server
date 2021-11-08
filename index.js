ffbd9f693cd0d0bcbc8200d34e6281f2aabfe2eb
Hasan75 <hasanjab14@gmail.com>
2021-11-08 23:41:22 +0600

    database connected and dotenv installed

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require ('express');
-const app = express();
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
+
+const app = express();
 const port = process.env.PORT || 5000;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4dxux.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+//console.log(uri)
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/',(req,res)=> {
     res.send('Running Genius Server');
