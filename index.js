437814f80458db469818dc51fdad15a5f177ff58
Hasan75 <hasanjab14@gmail.com>
2021-11-09 04:21:34 +0600

    Delete api used

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require ('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
 const cors = require('cors')
 require('dotenv').config();
 
@@ -31,6 +33,15 @@ async function run (){
             res.send(services);
         })
 
+        //GET Single Service
+        app.get('/services/:id', async(req,res)=>{
+            const id = req.params.id;
+            console.log('getting service', id);
+            const query = {_id: ObjectId(id)};
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
         //POST API
         app.post('/services', async(req,res) => {
             const service = req.body;
@@ -39,6 +50,14 @@ async function run (){
             const result = await servicesCollection.insertOne(service)
             console.log(result)
             res.json(result);
+        })
+
+        //DELETE API
+        app.delete('/services/:id', async(req,res)=> {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result)
         })
         
 
